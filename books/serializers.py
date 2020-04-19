e1ee8dced04686fe7eff548b218391f4678994c1
cristianjs19 <cristianjs19@gmail.com>
2020-04-19 12:17:55 -0300

    List books by user

--- books/serializers.py
+++ books/serializers.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers, fields, exceptions
 from books.models import Book, BookImage, LendingAgreement, LendingRequest
 
+class FilterBookByUserSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        try:
+            data = data.filter(user=self.context['request'].user)
+        except:
+            data = Book.objects.all()
+        return super(FilterBookByUserSerializer, self).to_representation(data)
 
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Order
+#         list_serializer_class = FilteredOrderSerializer
+
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     order_set = OrderSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = Product
 
 
 class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
+        list_serializer_class = FilterBookByUserSerializer
         fields = '__all__'
 
 class BookImageSerializer(serializers.ModelSerializer):
