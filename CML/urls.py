fc6ff32e05c167f8000f83edfa7efa6dfc0281cf
cristianjs19 <cristianjs19@gmail.com>
2020-04-30 00:50:34 -0300

    Add pyyml, API schema generator

--- CML/urls.py
+++ CML/urls.py
@@ -1,28 +1,13 @@
-"""CML URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 from .views import HomePageView
 from accounts.views import UserCreateView
 from django.contrib.auth.views import LoginView, LogoutView
 
-# from usuarios.urls import pages_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,7 +18,8 @@ urlpatterns = [
     
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
-    path('docs/', include_docs_urls(title='My API'))
+    path('docs/', include_docs_urls(title='Books API')),
+    path('schema/', get_schema_view(title="Books API"))
     # path('acuerdo/', include('books.urls')),
     # path('solicitud/', include('books.urls')),
     # path('accounts/', include('allauth.urls')),
